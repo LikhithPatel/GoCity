a0182b5926c58bc9fe07d0417f54b6843a443631
LikhithPatel <likithpatel14@gmail.com>
2026-08-29 21:35:56 +0530

    Added Ride Controller

--- src/main/java/com/likhith/gocity/controller/RideController.java
+++ src/main/java/com/likhith/gocity/controller/RideController.java
@@ -1,11 +1,53 @@
 package com.likhith.gocity.controller;
 
+import com.likhith.gocity.dto.RideSearchRequest;
+import com.likhith.gocity.entity.Ride;
+import com.likhith.gocity.service.RideService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/rides")
 @RequiredArgsConstructor
 public class RideController {
+    private final RideService rideService;
+
+    @PostMapping("/driver/{driverId}/vehicle/{vehicleId}")
+    public ResponseEntity<Ride> postRide(@PathVariable Long driverId, @PathVariable Long vehicleId, @RequestBody Ride ride){
+        return ResponseEntity.ok(rideService.postRide(driverId, vehicleId, ride));
+    }
+
+    @PostMapping("/search")
+    public ResponseEntity<List<Ride>> searchRide(@RequestBody RideSearchRequest  rideSearchRequest){
+        return ResponseEntity.ok(rideService.searchRides(rideSearchRequest));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Ride>> getAllScheduledRides(){
+        return ResponseEntity.ok(rideService.getAllScheduledRides());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Ride> getRideById(@PathVariable Long id){
+        return ResponseEntity.ok(rideService.getRideById(id));
+    }
+
+    @GetMapping("/driver/{driverId}")
+    public ResponseEntity<List<Ride>> getRidesByDriverId(@PathVariable Long driverId) {
+        return ResponseEntity.ok(rideService.getRidesByDriverId(driverId));
+    }
+
+    @PatchMapping("/{id}/cancel")
+    public ResponseEntity<Ride> cancelRide(@PathVariable Long id){
+        return ResponseEntity.ok(rideService.cancelRide(id));
+    }
+
+    @PatchMapping("/{id}/status")
+    public ResponseEntity<Ride> updateStatus(@PathVariable Long id, @RequestBody Ride.RideStatus rideStatus){
+        return ResponseEntity.ok(rideService.updateRideStatus(id, rideStatus));
+    }
+
 }
