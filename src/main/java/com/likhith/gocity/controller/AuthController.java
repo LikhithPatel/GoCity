761e14b12a642e62701ca2d134940afa3b1c377d
LikhithPatel <likithpatel14@gmail.com>
2026-09-10 22:39:07 +0530

    modified AuthController

--- src/main/java/com/likhith/gocity/controller/AuthController.java
+++ src/main/java/com/likhith/gocity/controller/AuthController.java
@@ -3,43 +3,25 @@ package com.likhith.gocity.controller;
 import com.likhith.gocity.dto.LoginRequest;
 import com.likhith.gocity.dto.RegisterRequest;
 import com.likhith.gocity.entity.Profile;
-import com.likhith.gocity.exception.ApiException;
-import com.likhith.gocity.repository.ProfileRepository;
+import com.likhith.gocity.service.AuthService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@Service
+@RestController
+@RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class AuthController {
-    private final ProfileRepository profileRepository;
-    private final PasswordEncoder passwordEncoder;
 
-    public Profile Register(RegisterRequest request){
-        profileRepository.findByEmail(request.getEmail()).ifPresent(p->{
-            throw new ApiException("An account with this email already exists");
-        });
+    private final AuthService authService;
 
-        if(request.getPassword()==null || request.getPassword().length()<6){
-            throw new ApiException("Password length less than 6 characters");
-        }
-
-        Profile profile=new Profile();
-        profile.setEmail(request.getEmail());
-        profile.setPassword(passwordEncoder.encode(request.getPassword()));
-        profile.setFullName(request.getFullName());
-        profile.setPhoneNumber(request.getPhoneNumber());
-
-        return profileRepository.save(profile);
+    @PostMapping("/register")
+    public ResponseEntity<Profile> register(@RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(authService.register(request));
     }
 
-    public Profile Login(LoginRequest request){
-        Profile profile=profileRepository.findByEmail(request.getEmail()).orElseThrow(()->
-                new ApiException("Invalid email or password"));
-
-        if(!passwordEncoder.matches(request.getPassword(),profile.getPassword())){
-            throw new ApiException("Invalid email or password");
-        }
-        return profile;
+    @PostMapping("/login")
+    public ResponseEntity<Profile> login(@RequestBody LoginRequest request) {
+        return ResponseEntity.ok(authService.login(request));
     }
 }
